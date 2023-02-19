1dcb621fddacf502a53097293459c16305da026d
jaka2005 <jakalogin1808@gmail.com>
2023-02-19 15:18:29 +0400

    commit before separate to branches

--- backend/src/main/kotlin/com/j2k/plugins/Routing.kt
+++ backend/src/main/kotlin/com/j2k/plugins/Routing.kt
@@ -1,21 +1,36 @@
 package com.j2k.plugins
 
 import freemarker.cache.ClassTemplateLoader
+import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
 import io.ktor.server.freemarker.*
 import io.ktor.server.http.content.*
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.InputStreamReader
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondRedirect("/index.html")
+            call.respondText(
+                getResourceFileContent("files/index.html"),
+                ContentType.Text.Html,
+                HttpStatusCode.OK
+            )
+        }
+
+        get("/our-team") {
+            call.respondText(
+                getResourceFileContent("files/our-team.html"),
+                ContentType.Text.Html,
+                HttpStatusCode.OK
+            )
         }
         
         static("/") {
             resources("files")
-            default("index.html")
         }
     }
 }
@@ -24,4 +39,13 @@ fun Application.configureTemplating() {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
     }
+}
+
+fun getResourceFileContent(fileName: String): String {
+    val classLoader = Thread.currentThread().contextClassLoader
+    val inputStream = classLoader.getResourceAsStream(fileName)
+        ?: throw FileNotFoundException("file $fileName not found")
+    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+
+    return bufferedReader.use { it.readText() }
 }
