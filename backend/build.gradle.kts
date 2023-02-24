ca9cb04017dd4b69ef4ce7c1d9c41f193d3b060a
jaka2005 <jakalogin1808@gmail.com>
2023-02-24 15:07:27 +0400

    added status page Routing.kt (404 error)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -24,7 +24,10 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-freemarker:$ktor_version")
+    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-host-common-jvm:2.2.3")
+    implementation("io.ktor:ktor-server-status-pages-jvm:2.2.3")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
